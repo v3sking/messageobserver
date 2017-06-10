4ff17995247f213dc12bec5143ce3ce23df6ab42
v3sking <250985725@qq.com>
2017-06-10 21:04:47 +0800

    增加App注释
    Signed-off-by: v3sking <250985725@qq.com>

--- src/main/java/com/lhy/boot/messageobserver/App.java
+++ src/main/java/com/lhy/boot/messageobserver/App.java
@@ -4,6 +4,11 @@ package com.lhy.boot.messageobserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 如果不配置scanBasePackages默认扫描当前包目录
+ * @author luanhy
+ *
+ */
 @SpringBootApplication
 public class App {
 	public static void main(String[] args) {
